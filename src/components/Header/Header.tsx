b07d7ba486fd3774d19fe92117ed0af94fea3247
Bergwolf4880 <poprjaduhha.ivan@gmail.com>
2024-05-28 10:25:52 +0200

    header elements

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { HeaderComponent, HeaderMotto, HeaderText, NavMenu, SeparationLine } from './styles'
+
+export const Header = () => {
+    
+
+    return (
+        <HeaderComponent>
+            <HeaderMotto>
+            Text in Header
+            </HeaderMotto> 
+            <NavMenu>            
+            <HeaderText>MAP</HeaderText>
+            <HeaderText>USER AREA</HeaderText>
+            <HeaderText>LOGIN</HeaderText>
+            </NavMenu> 
+            <SeparationLine />
+        </HeaderComponent>
+    )
+}
---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled'
+
+
+export const LayoutComponent = styled.div`
+display: flex;
+flex:1;
+height: fit-content;
+
+`
+
+export const HeaderComponent = styled.header`
+display: flex;
+width: auto;
+height:400px;
+
+background-color: blue;
+`
+
+export const FooterComponent = styled.footer`
+display: flex;
+width: 100%;
+height: 400px;
+`
