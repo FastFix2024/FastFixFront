c72e2a68e46b1df9e59a85ef89f42f7568ae89af
Erbol <yers2106gmail.com>
2024-06-10 14:33:14 +0600

    Merge demo branch with the following changes: Improved car wash
    Added react-icons
    
    Added reviews
    
    Added website
    
    Added star rating

--- src/components/MapWrapperTest/styles.ts
+++ src/components/MapWrapperTest/styles.ts
@@ -51,11 +51,18 @@ export const ServiceButton = styled(Button)`
   color: #000;
   border: 1px solid #ccc;
   margin-right: 8px;
+  display: flex;
+  align-items: center;
+
   &:last-of-type {
     margin-right: 0;
   }
   &:disabled {
     cursor: not-allowed;
+  }
+
+  svg {
+    margin-right: 5px;
   }
 `;
 
@@ -65,8 +72,6 @@ export const MapContainer = styled.div`
   align-items: center;
 `;
 
-
-
 export const UserMarkerToggle = styled.button`
   padding: 8px 12px;
   border: none;
@@ -75,6 +80,12 @@ export const UserMarkerToggle = styled.button`
   color: #fff;
   cursor: pointer;
   z-index: 1;
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-right: 5px;
+  }
 `;
 
 export const PlacesList = styled.div`
@@ -92,17 +103,27 @@ export const PlacesList = styled.div`
 
 export const PlaceItem = styled.div`
   display: flex;
+  justify-content: space-between;
   padding: 10px;
   border-bottom: 1px solid #ccc;
   cursor: pointer;
 `;
 
-export const PlacePhoto = styled.img`
+export const PlacePhoto = styled.div`
   width: 100px;
   height: 100px;
-  object-fit: cover;
-  margin-right: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
   border-radius: 4px;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
 
 export const PlaceItemContent = styled.div`
@@ -129,6 +150,13 @@ export const RouteButton = styled.button`
   cursor: pointer;
   margin-top: 10px;
   width: 100%;
+  max-width: 150px;
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-right: 5px;
+  }
 `;
 
 export const CloseResultsButtonContainer = styled.div`
@@ -149,7 +177,38 @@ export const CloseResultsButton = styled.button`
   font-size: 1rem;
 `;
 
-
 export const MapWindowContainer = styled.div`
   position: relative;
+`;
+
+export const ReviewsContainer = styled.div`
+  position: absolute;
+  top: -147px;
+  left: calc(10% + 320px);
+  width: 300px;
+  height: 800px;
+  overflow-y: auto;
+  background-color: #fff;
+  z-index: 1;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  padding: 20px;
+`;
+
+export const ReviewItem = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const ReviewAuthor = styled.p`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+export const ReviewRating = styled.p`
+  margin-bottom: 5px;
+`;
+
+export const ReviewText = styled.p`
+  font-size: 0.9rem;
+  line-height: 1.4;
 `;
