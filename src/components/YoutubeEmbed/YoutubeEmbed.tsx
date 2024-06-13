a6e2d800fd513c4848f35dea7fd63539776c3885
Bergwolf4880 <poprjaduhha.ivan@gmail.com>
2024-06-13 04:10:08 +0200

    design fixes, car facts, fuelAPI, Fuel cards, reminder content

--- src/components/YoutubeEmbed/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed/YoutubeEmbed.tsx
@@ -1,12 +1,13 @@
-import { VideoContainer } from './styles'
-
+import { VideoContainer } from "./styles";
 
 const YoutubeEmbed = () => (
   <VideoContainer>
     <iframe
-    width="640"
-    height="360"
-      src="https://www.youtube.com/embed/R-F38m8k_u8?disablekb=1&enablejsapi=1&fs=0&modestbranding=1&color=white&iv_load_policy=3"
+      width="640"
+      height="360"
+      src="https://www.youtube.com/embed/R-F38m8k_u8?rel=0&disablekb=1&enablejsapi=1&fs=0&modestbranding=1&color=white&iv_load_policy=3&autoplay=0&mute=0&loop=1&controls=0&color=white&
+      modestbranding=0&showinfo=0&playsinline=1&enablejsapi=1&
+      playlist=PLR-F38m8k_u8"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
@@ -15,4 +16,5 @@ const YoutubeEmbed = () => (
   </VideoContainer>
 );
 
-export default YoutubeEmbed
+export default YoutubeEmbed;
+
